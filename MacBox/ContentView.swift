e4ebf2958813ee3ebc08f0a2afa809a4879a5f2f
zhihaofans <zhihaofans@hotmail.com>
2025-11-14 00:02:48 +0800

    Initial Commit

---
+++ MacBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacBox
+//
+//  Created by zzh on 2025/11/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacBox/MacBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacBoxApp.swift
+//  MacBox
+//
+//  Created by zzh on 2025/11/14.
+//
+
+import SwiftUI
+
+@main
+struct MacBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
